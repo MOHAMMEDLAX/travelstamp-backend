fb8710b113b110927f106170eece4bf937025a99
MOHAMMED lachheb <mohammedlachheb19@gmail.com>
2026-03-12 22:23:14 +0100

    update cors for production

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['https://travelstamp-app.vercel.app'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
