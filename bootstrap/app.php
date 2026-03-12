f118b335fa200f6c73112f2fde61249f4a290125
MOHAMMED lachheb <mohammedlachheb19@gmail.com>
2026-03-13 00:05:16 +0100

    use built-in cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,8 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-  ->withMiddleware(function (Middleware $middleware): void {
-    $middleware->prepend(\App\Http\Middleware\CorsMiddleware::class);
+->withMiddleware(function (Middleware $middleware): void {
+    $middleware->api(prepend: [
+        \Illuminate\Http\Middleware\HandleCors::class,
+    ]);
 })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
