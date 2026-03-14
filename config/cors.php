c46a283df25faec0ed3359288b0a334947282aee
MOHAMMED lachheb <mohammedlachheb19@gmail.com>
2026-03-14 20:50:07 +0100

    fix cors

--- config/cors.php
+++ config/cors.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    'paths' => ['api/*'],
-    'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'],
+    'paths'                    => ['api/*'],
+    'allowed_methods'          => ['*'],
+    'allowed_origins'          => ['https://travelstamp-bojy.vercel.app'],
     'allowed_origins_patterns' => [],
-    'allowed_headers' => ['*'],
-    'exposed_headers' => [],
-    'max_age' => 0,
-    'supports_credentials' => false,
+    'allowed_headers'          => ['*'],
+    'exposed_headers'          => [],
+    'max_age'                  => 0,
+    'supports_credentials'     => true,
 ];
